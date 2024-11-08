9c8e4dd054a26f1367c6a4c2b2a57cad65f77d18
xolani-tech <jonathanmicah23@gmal.com>
2024-11-08 11:15:14 +0200

    hey there

---
+++ Exercise 5 (1).py
@@ -0,0 +1,51 @@
+# Question 1: Creating and Modifying Lists
+
+# TODO: Create a list of fruits
+
+# TODO: Add a fruit to the end of the list
+
+# TODO: Insert a fruit at the beginning of the list
+
+# TODO: Remove a fruit from the list
+
+# TODO: Print the modified list
+
+
+#-------------------------------------------------------------------------
+# Question 2: List Operations
+
+# TODO: Create a list of numbers from 1 to 5
+
+# TODO: Create a new list with each number squared
+
+# TODO: Find the sum and average of the original numbers
+
+# TODO: Print the results
+
+
+#-------------------------------------------------------------------------
+# Question 3: Creating and Modifying Dictionaries
+
+# TODO: Create a dictionary of countries and their capitals
+
+# TODO: Add a new country-capital pair
+
+# TODO: Update the capital of an existing country
+
+# TODO: Remove a country-capital pair
+
+# TODO: Print the modified dictionary
+
+
+#-------------------------------------------------------------------------
+# Question 4: Dictionary Operations
+
+# TODO: Create a dictionary of fruit colors
+
+# TODO: Print all the fruits (keys)
+
+# TODO: Print all the colors (values)
+
+# TODO: Print each fruit and its color
+
+# TODO: Check if a fruit is in the dictionary and print its color
